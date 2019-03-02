e09a05369d9b3ec5f30585addc820ff02f3a2a3f
Hubert Skrzypczak <hskrzypczak98@gmail.com>
2019-03-02 13:34:10 +0100

    Implement obs preprocessing and update learnign process

--- examples/learning_run.py
+++ examples/learning_run.py
@@ -4,46 +4,102 @@ import tensorflow as tf
 
 def main():
     # Create agent able to learn
-    model = create_model()
-    dqn_agent = agents.DQN_agent(model)
-    dqn_agent_index = 1
+    # model = create_model()
+    # dqn_agent = agents.DQN_agent(model, epsilon=0.5)
+    # dqn_agent_index = 0
+
+    # create four models to check which is winning most times
+    models = create_four_test_models()
+    dqn_agents = []
+    for i in range(4):
+        dqn_agents.append(agents.DQN_agent(models[i], epsilon=0.5))
 
     # Create a set of two agents
     agent_list = [
-        agents.SimpleAgent(),
-        dqn_agent
+        dqn_agents[0],
+        dqn_agents[1],
+        dqn_agents[2],
+        dqn_agents[3]
     ]
 
     # Make the "Free-For-All" environment using the agent list
-    env = pommerman.make('PommeFFACompetition-v0', agent_list)
+    env = pommerman.make('PommeFFACompetitionFast-v0', agent_list)
+    # env = pommerman.make('PommeFFACompetition-v0', agent_list)
 
     # Run the episodes just like OpenAI Gym
-    for episode in range(5):
+    for episode in range(30):
+        steps_per_episode = 0
         state = env.reset()
         done = False
         while not done:
             # env.render()
             actions = env.act(state)
             new_state, reward, done, info = env.step(actions)
-
-            # agent learning
-            dqn_agent.remember(
-                state[dqn_agent_index], actions[dqn_agent_index],
-                reward[dqn_agent_index], done, new_state[dqn_agent_index]
-            )
-            dqn_agent.experience_replay()
+            steps_per_episode += 1
+            # agents learning
+            for i in range(4):
+                dqn_agents[i].remember(
+                    state[i], actions[i],
+                    reward[i], done, new_state[i]
+                )
+                dqn_agents[i].experience_replay()
 
             state = new_state
 
-        print('Episode {} finished'.format(episode))
+        print('Episode {} finished\n steps: {}\ninfo: {}'.
+            format(episode, steps_per_episode, info))
+        # saving weights after every epoch
+        for i, dqn_agent in enumerate(dqn_agents):
+            dqn_agent.save_weights("./weights/model_{}_episode_{}".format(i, episode))
     env.close()
 
-
-def create_model():
-    model = tf.keras.Sequential([
+def create_four_test_models():
+    model1 = tf.keras.Sequential([
+        tf.keras.layers.Conv2D(8, input_shape=(11, 11, 6),kernel_size=(5, 5), activation='relu', padding='same'),
         tf.keras.layers.Flatten(),
         tf.keras.layers.Dense(64, activation='relu'),
         tf.keras.layers.Dense(64, activation='relu'),
+        tf.keras.layers.Dense(6, activation='softmax')
+    ])
+    model1.compile(optimizer=tf.train.AdamOptimizer(0.001), loss='mse', metrics=['mae'])
+
+    model2 = tf.keras.Sequential([
+        tf.keras.layers.Conv2D(8, input_shape=(11, 11, 6), kernel_size=(5, 5), activation='relu'),
+        tf.keras.layers.Conv2D(16, kernel_size=(5, 5), activation='relu'),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(64, activation='relu'),
+        tf.keras.layers.Dense(64, activation='relu'),
+        tf.keras.layers.Dense(6, activation='softmax')
+    ])
+    model2.compile(optimizer=tf.train.AdamOptimizer(0.001), loss='mse', metrics=['mae'])
+
+    model3 = tf.keras.Sequential([
+        tf.keras.layers.Conv2D(8, input_shape=(11, 11, 6), kernel_size=(5, 5), activation='relu', padding='same'),
+        tf.keras.layers.Conv2D(16, input_shape=(11, 11, 6), kernel_size=(5, 5), activation='relu', padding='same'),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(100, activation='relu'),
+        tf.keras.layers.Dense(6, activation='softmax')
+    ])
+    model3.compile(optimizer=tf.train.AdamOptimizer(0.001), loss='mse', metrics=['mae'])
+
+    model4 = tf.keras.Sequential([
+        tf.keras.layers.Conv2D(8, input_shape=(11, 11, 6), kernel_size=(3, 3), activation='relu'),
+        tf.keras.layers.Conv2D(16, input_shape=(11, 11, 6), kernel_size=(3, 3), activation='relu'),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(64, activation='relu'),
+        tf.keras.layers.Dense(64, activation='relu'),
+        tf.keras.layers.Dense(6, activation='softmax')
+    ])
+    model4.compile(optimizer=tf.train.AdamOptimizer(0.001), loss='mse', metrics=['mae'])
+
+    return model1, model2, model3, model4
+
+def create_model():
+    model = tf.keras.Sequential([
+        tf.keras.layers.Conv2D(8, input_shape=(11, 11, 6), kernel_size=(5, 5), activation='relu', padding='same'),
+        tf.keras.layers.Conv2D(16, input_shape=(11, 11, 6), kernel_size=(5, 5), activation='relu', padding='same'),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(100, activation='relu'),
         tf.keras.layers.Dense(6, activation='softmax')
     ])
     model.compile(optimizer=tf.train.AdamOptimizer(0.001), loss='mse', metrics=['mae'])
--- pommerman/agents/dqn_agent.py
+++ pommerman/agents/dqn_agent.py
@@ -8,27 +8,27 @@ import random
 
 class DQN_agent(BaseAgent):
     
-    def __init__(self, model):
+    def __init__(self, model, gamma=0.95, memory_length=10000, 
+        epsilon=1.0, ep_decay=0.999, batch_size=32):
         super(DQN_agent, self).__init__()
         self.model = model
-        self.memory = deque(maxlen=100000)
+        self.memory = deque(maxlen=memory_length)
         # set hyperparameters
-        self.gamma = 0.95
-        self.epsilon = 1.0
-        self.ep_decay = 0.999
-        self.batch_size = 32
+        self.gamma = gamma
+        self.epsilon = epsilon
+        self.ep_decay = ep_decay
+        self.batch_size = batch_size
 
     # TODO: modify state format (for now it's only board, without any other features)
     def act(self, obs, action_space):
         if random.random() < self.epsilon:
             a = action_space.sample()
         else:
-            a = np.argmax(self.model.predict(np.array([obs['board']])))
+            a = np.argmax(self.model.predict(np.array([self.preprocess_observation(obs)])))
         return a
         
-    # TODO: modify state format (for now it's only board, without any other features)
-    def remember(self, old_state, action, reward, done, new_state):
-        self.memory.append((old_state['board'], action, reward, done, new_state['board']))
+    def remember(self, obs, action, reward, done, new_obs):
+        self.memory.append((self.preprocess_observation(obs), action, reward, done, self.preprocess_observation(new_obs)))
         if done:
             self.epsilon *= self.ep_decay
     
@@ -36,13 +36,59 @@ class DQN_agent(BaseAgent):
     def experience_replay(self):
         if len(self.memory) > self.batch_size:
             batch = random.sample(self.memory, self.batch_size)
+            q_vals = []
+            old_states = []
             for old_state, action, reward, done, new_state in batch:
+                old_states.append(old_state)
                 q_update = reward
                 if not done:
                     q_update += self.gamma * np.max(self.model.predict(np.array([new_state])))
                 q_val = self.model.predict(np.array([old_state]))[0]
                 q_val[action] = q_update
-                self.model.fit(np.array([old_state]), np.array([q_val]), verbose=0)
+                q_vals.append(q_val)
+            old_states = np.array(old_states)
+            q_vals = np.array(q_vals)
+            self.model.fit(old_states, q_vals, verbose=0)
+
+    # prepare state as stack of 11x11 matrices
+    def preprocess_observation(self, obs):
+        # list for matrices
+        prepared_features_list = []
+
+        # board matrixs with enemies as 11 and agent as 10
+        board_features = obs['board']
+        agent_x, agent_y = obs['position']
+        for i in range(len(board_features)):
+            for j in range(len(board_features[i])):
+                if 10 <= board_features[i][j] <= 13:
+                    board_features[i][j] = 11
+                if i == agent_y and j == agent_x:
+                    board_features[i][j] = 10
+        board_features = tf.constant(board_features, dtype=tf.float32)
+        board_features = tf.reshape(board_features, (11, 11, 1))
+        prepared_features_list.append(board_features)
+    
+        # bomb_blast_strength and bomb_life matrices
+        sparse_features = ['bomb_blast_strength', 'bomb_life']
+        for feature in sparse_features:
+            prepared_feature = tf.constant(obs[feature], dtype=tf.float32)
+            prepared_feature = tf.reshape(prepared_feature, (11, 11, 1))
+            prepared_features_list.append(prepared_feature)
+        # blast strength as a matrix
+        blast_strength_feature = tf.ones((11, 11, 1), dtype=tf.float32) * obs['blast_strength']
+        prepared_features_list.append(blast_strength_feature)
+        # can kick as a matrix
+        can_kick = 1 if obs['can_kick'] else 0.0
+        can_kick_feature = tf.ones((11, 11, 1), dtype=tf.float32) * can_kick
+        prepared_features_list.append(can_kick_feature)
+        # ammo number as a matrix
+        ammo_feature = tf.ones((11, 11, 1), dtype=tf.float32) * obs['ammo']
+        prepared_features_list.append(ammo_feature)
+        # run session on prepared_features_list to have an actual values  
+        with tf.Session() as sess:
+            state = sess.run(tf.concat(prepared_features_list, axis=2))
+            sess.close()
+        return state
 
     def set_model(self, model):
         self.model = model
@@ -55,3 +101,4 @@ class DQN_agent(BaseAgent):
         
     def restore_weights(self, file_name):
         self.model.load_weights(file_name)
+
